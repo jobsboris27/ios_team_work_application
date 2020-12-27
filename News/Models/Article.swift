6001dd8e66d891b1298129d2b868bda62c58feff
Boris <jobsboris27@gmail.com>
2020-12-27 17:12:02 +0300

    wip: add models

---
+++ News/Models/Article.swift
@@ -0,0 +1,15 @@
+//
+//  Article.swift
+//  News
+//
+//  Created by Boris on 27.12.2020.
+//
+
+import Foundation
+
+struct Article {
+  let image: String
+  let title: String
+  let text: String
+  let date: Date
+}
---
+++ News/Models/Currency.swift
@@ -0,0 +1,13 @@
+//
+//  Currency.swift
+//  News
+//
+//  Created by Boris on 27.12.2020.
+//
+
+import Foundation
+
+struct Currency {
+  let name: String
+  let value: String
+}
