ece51cbc54b661e7701879aee157e1aa3dd269c1
Eugene <prokhorov.eu@icloud.com>
2021-01-05 00:14:38 +0400

    1.1

---
+++ News/VC/PageNewsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PageNewsViewController.swift
+//  NewsApp
+//
+//  Created by Евгений Прохоров on 04.01.2021.
+//
+
+import UIKit
+
+class PageNewsViewController: UIViewController {
+
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var textView: UITextView!
+    @IBOutlet var readMoreButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func tappedButton(_ sender: Any) {
+    }
+
+}
