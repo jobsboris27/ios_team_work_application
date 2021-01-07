5e42e9a0479318fa842f3c2e42b5897eca19b64c
Eugene <prokhorov.eu@icloud.com>
2021-01-07 18:09:07 +0400

    1.2

---
+++ News/Screens/CurrencyCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CurrencyCollectionViewCell.swift
+//  News
+//
+//  Created by Евгений Прохоров on 07.01.2021.
+//
+
+import UIKit
+
+class CurrencyCollectionViewCell: UICollectionViewCell {
+    
+}
