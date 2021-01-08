998106926d60d93bc52a5db6c6237a633da51875
VerKse <ksenofontovs.cs@gmail.com>
2021-01-09 01:08:31 +0300

    FeedController crash fixed

--- News/Views/FeedController/FeedController.swift
+++ News/Views/FeedController/FeedController.swift
@@ -1,16 +1,38 @@
 //
 //  FeedController.swift
-//  NewsApp
+//  News
 //
-//  Created by Вера Ксенофонтова on 01.01.2021.
+//  Created by Вера Ксенофонтова on 09.01.2021.
 //
 
 import UIKit
 
-class FeedController: UIViewController {
+class FeedController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "feedCell", for: indexPath)
+            
+//            if let feedCell = cell as? FeedTableViewCell {
+//                feedCell.feedLabel.text = "index \(indexPath)"
+//                feedCell.preview.image = UIImage(named: "default")
+//                feedCell.dateLabel.text = "date"
+//                return feedCell
+//            }
+            
+            return cell
+    }
+    
 
+//    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        tableView.dataSource = self
+//        tableView.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -18,4 +40,3 @@ class FeedController: UIViewController {
     
     }
 }
-
--- News/Views/FeedController/FeedTableViewCell.swift
+++ News/Views/FeedController/FeedTableViewCell.swift
@@ -1,27 +1,23 @@
 //
 //  FeedTableViewCell.swift
-//  NewsApp
+//  News
 //
-//  Created by Вера Ксенофонтова on 03.01.2021.
+//  Created by Вера Ксенофонтова on 09.01.2021.
 //
 
 import UIKit
 
 class FeedTableViewCell: UITableViewCell {
-    
-    @IBOutlet weak var preview: UIImageView!
-    @IBOutlet weak var categotyLabel: UILabel!
-    @IBOutlet weak var feedLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         // Initialization code
     }
-    
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
     }
-    
+
 }
